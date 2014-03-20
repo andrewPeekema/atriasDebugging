cecb9661b0fa55aea04ce4a80941e249ef8ea642
Andrew Peekema <apeekema@gmail.com>
2014-03-19 17:07:33 -0700

    Starting power calculations

---
+++ powerCalculations.m
@@ -0,0 +1,25 @@
+% Cleanup
+close all
+clc
+
+% With the sine wave controller, right leg B moves
+% Electrical Power
+voltage = 12*3; % V
+current = (0.5+v_log__robot__state_currentPositive)*-1; % A
+electricalPower = voltage*current;
+
+% Mechanical Power
+torqueConstant = 0.121;
+motorVelocity  = v_log__robot__state_rBMotorVelocity;
+motorTorque = torqueConstant*v_log__robot__state_rBClampedCmd;
+mechPower   = motorVelocity.*motorTorque;
+
+% Electrical power loss
+km = 0.529; % N*m/sqrt.w
+powerLoss = (motorTorque/km).^2; % w
+
+% Display data
+plot(electricalPower,'b.')
+hold on
+plot(mechPower,'r.')
+plot(powerLoss,'g.')
