6838ae62318d399a07c9e3e7168b9426240002c0
Andrew Peekema <apeekema@gmail.com>
2014-04-01 21:47:03 -0700

    Updated the power calculations to use the new voltage sensor

--- statistics/powerCalculations.m
+++ statistics/powerCalculations.m
@@ -4,9 +4,9 @@ clc
 
 % With the sine wave controller, right leg B moves
 % Electrical Power
-voltage = 12*3; % V
+voltage = v_log__robot__state_lHipMotorVoltage; % V
 current = v_log__robot__state_currentPositive; % A
-electricalPower = voltage*current;
+electricalPower = voltage.*current;
 
 % Mechanical Power
 torqueConstant = 0.121;
@@ -15,15 +15,10 @@ motorVelocity  = gearRatio*v_log__robot__state_rBMotorVelocity;
 motorTorque = torqueConstant*v_log__robot__state_rBClampedCmd;
 mechPower   = motorVelocity.*motorTorque;
 
-% Electrical power loss
-km = 0.529; % N*m/sqrt.w
-powerLoss = (motorTorque/km).^2; % w
-
 % Display data
 plot(electricalPower,'b.')
 hold on
 plot(mechPower,'r.')
-%plot(powerLoss,'g.')
 
 title('Positive System Power')
 xlabel('Time (ms)')
