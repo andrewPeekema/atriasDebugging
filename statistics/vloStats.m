80cf1ec8a55f0e28a3fb051185c1b1f67dd35eb6
Andrew Peekema <apeekema@gmail.com>
2014-04-23 14:25:29 -0700

    Added energy calculations to vloStats.m

--- statistics/vloStats.m
+++ statistics/vloStats.m
@@ -1,6 +1,7 @@
 % Find:
 %   Vertical Leg Orientation height
 %   Vertical Leg Orientation velocity
+%   Vertical Leg Orientation energy
 
 % Cleanup
 clc
@@ -39,6 +40,12 @@ for n = 1:length(td)
     dx = a.Kinematics.velocity(nRApex(n),1);
     dy = a.Kinematics.velocity(nRApex(n),2);
     vRApex(n) = hypot(dx,dy);
+    % Motor and body angular velocity
+    dqLA_RApex(n) = a.Kinematics.motors.MotorVelocity(nRApex(n),1);
+    dqLB_RApex(n) = a.Kinematics.motors.MotorVelocity(nRApex(n),2);
+    dqRA_RApex(n) = a.Kinematics.motors.MotorVelocity(nRApex(n),3);
+    dqRB_RApex(n) = a.Kinematics.motors.MotorVelocity(nRApex(n),4);
+    dq_RApex(n)   = a.Kinematics.bodyPitchVelocity(nRApex(n),1);
 end
 
 %% Left leg
@@ -71,6 +78,12 @@ for n = 1:length(td)
     dx = a.Kinematics.velocity(nLApex(n),1);
     dy = a.Kinematics.velocity(nLApex(n),2);
     vLApex(n) = hypot(dx,dy);
+    % Motor and body angular velocity
+    dqLA_LApex(n) = a.Kinematics.motors.MotorVelocity(nLApex(n),1);
+    dqLB_LApex(n) = a.Kinematics.motors.MotorVelocity(nLApex(n),2);
+    dqRA_LApex(n) = a.Kinematics.motors.MotorVelocity(nLApex(n),3);
+    dqRB_LApex(n) = a.Kinematics.motors.MotorVelocity(nLApex(n),4);
+    dq_LApex(n)   = a.Kinematics.bodyPitchVelocity(nLApex(n),1);
 end
 
 %figure
@@ -94,20 +107,77 @@ stats.vRApexStd  = std(vRApex);
 stats.vApexMean  = mean([vLApex vRApex]);
 stats.vApexStd   = std([vLApex vRApex]);
 
-%{
-% TODO: Finish energy calculations
-% VLO Energy
+
+
+%% VLO Energy
 addpath('./..') % for legForce()
+% E = KE + PEg + PEs
 % E = 1/2*m*v^2 + m*g*h + 1/2*k*Dx^2
-% Spring potential energy
-r  = rRApex;
-%r = a.Kinematics.legLength(1:end,2);
+
+% Constants
+g = 9.81; % Gravity (m/s^2)
+m = 60;   % Mass (kg)
+Ir = 50^2*0.0019; % Reflected rotor moment of inertia (kg*m^2)
+It = 4.1; % Torso moment of inertia (kg*m^2)
+
+% Spring potential energy (PEs)
+nApices = length([nLApex nRApex]);
+if nLApex(1) < nRApex(1)
+    display('First apex is on the left leg')
+    r(1:2:nApices) = rLApex;
+    r(2:2:nApices) = rRApex;
+    h(1:2:nApices) = yLApex;
+    h(2:2:nApices) = yRApex;
+    v(1:2:nApices) = vLApex;
+    v(2:2:nApices) = vRApex;
+    dq(1:2:nApices) = dq_LApex;
+    dq(2:2:nApices) = dq_RApex;
+    dqLA(1:2:nApices) = dqLA_LApex;
+    dqLA(2:2:nApices) = dqLA_RApex;
+    dqLB(1:2:nApices) = dqLB_LApex;
+    dqLB(2:2:nApices) = dqLB_RApex;
+    dqRA(1:2:nApices) = dqRA_LApex;
+    dqRA(2:2:nApices) = dqRA_RApex;
+    dqRB(1:2:nApices) = dqRB_LApex;
+    dqRB(2:2:nApices) = dqRB_RApex;
+else
+    display('First apex is on the right leg')
+    r(1:2:nApices) = rRApex;
+    r(2:2:nApices) = rLApex;
+    h(1:2:nApices) = yRApex;
+    h(2:2:nApices) = yLApex;
+    v(1:2:nApices) = vRApex;
+    v(2:2:nApices) = vLApex;
+    dq(1:2:nApices) = dq_RApex;
+    dq(2:2:nApices) = dq_LApex;
+    dqLA(1:2:nApices) = dqLA_RApex;
+    dqLA(2:2:nApices) = dqLA_LApex;
+    dqLB(1:2:nApices) = dqLB_RApex;
+    dqLB(2:2:nApices) = dqLB_LApex;
+    dqRA(1:2:nApices) = dqRA_RApex;
+    dqRA(2:2:nApices) = dqRA_LApex;
+    dqRB(1:2:nApices) = dqRB_RApex;
+    dqRB(2:2:nApices) = dqRB_LApex;
+end
+% The rest leg length
 r0 = 0.9; % TODO: a.ControllerData.r0(end);
 display('WARNING: Using a fixed r0.  This is a hack')
-Dx = r0-r
-%plot(Dx)
-k  = legForce(r, r0)./Dx;
+% If the leg length is longer than the rest leg length, set the PE to zero
+r(r>r0) = r0;
+% The deflection
+Dx = r0-r;
+% The spring constant
+k = legForce(r, r0)./Dx;
+% The spring potential energy
 PEs = 1/2*k.*Dx.^2;
-%}
 
-display(stats)
+% Kenetic Energy (KE)
+KE = 1/2*m*v.^2 + 1/2*It*dq.^2 + 1/2*Ir*(dqLA.^2 + dqLB.^2 + dqRA.^2 + dqRB.^2);
+
+% Gravitational Potential Energy (PEg)
+PEg = m*g*h;
+
+% Total energy
+E = KE + PEg + PEs
+
+%display(stats)
