010db01d27d51c3aae73bc5f0af3a382571c05d2
Andrew Peekema <apeekema@gmail.com>
2014-06-06 18:08:33 -0700

    Added more datasets in forceplateAnalysis

--- forceplateAnalysis.m
+++ forceplateAnalysis.m
@@ -1,3 +1,4 @@
+function forceplateAnalysis
 % Plot data from the forceplate alongside data from the robot
 
 % Cleanup
@@ -5,52 +6,61 @@ close all
 clear all
 clc
 
-% The logfile to analyze
+% The data directory
 directory = '/Users/andrew/Desktop/Force Control Testing/';
+
+% Analyze a logfile
 filePath = [directory 'atrias_2014-06-06-12-59-10.mat'];
 filePath2 = [directory 'force_test00007.txt'];
-[c rs cs] = logfileToStruct(filePath,filePath2);
-
-% Crop to the force test
-rs = shortenData(rs,[25490:50730]);
-cs = shortenData(cs,[25490:50730]);
-
-% Fit the force data
-bestFit = fitForceData(rs);
-
-% Find the world forces
-force = rsToWorldForce(rs, bestFit.ks);
-LFz = bestFit.y0 - force.LF.Fz;
-
-
-
-figure
-plot(rs.time, rs.forceplateF,'.');
-hold on
-%plot(rs.time, cs.fzDes,'b')
-plot(rs.time, -cs.computeFzL,'r')
-plot(rs.time, LFz,'g')
-
+fitAndPlotForces(filePath,filePath2,[25490:50730]);
 title('Feedback Linearization Force Control')
-xlabel('Time (s)')
-ylabel('Force (N)')
 
+% Analyze another logfile
+filePath = [directory 'atrias_2014-06-05-14-28-14.mat'];
+filePath2 = [directory 'force_test00004.txt'];
+fitAndPlotForces(filePath,filePath2,[20160:41520]);
+title('Feedback Linearization Force Control')
 
-%{
-%% The logfile to analyze
+% Analyze another logfile
 filePath = [directory 'atrias_2014-06-06-12-48-14.mat'];
 filePath2 = [directory 'force_test00006.txt'];
-[c rs cs] = logfileToStruct(filePath,filePath2);
-
-%rs = shortenData(rs,[32310:35800]);
-
-figure
-plot(rs.time, rs.forceplateF,'.');
-hold on
-plot(rs.time, -cs.controlFzL,'b')
-plot(rs.time, -cs.computeFzL,'r')
-
+fitAndPlotForces(filePath,filePath2,[22480:45400]);
 title('PD Force Control')
-xlabel('Time (s)')
-ylabel('Force (N)')
-%}
+
+% Analyze another logfile
+filePath = [directory 'atrias_2014-06-05-14-34-34.mat'];
+filePath2 = [directory 'force_test00005.txt'];
+fitAndPlotForces(filePath,filePath2,[19950:46180]);
+title('PD Force Control')
+
+function fitAndPlotForces(fp, fp2, shortenI)
+    [c rs cs] = logfileToStruct(fp,fp2);
+
+    % If the data should be shortened
+    if ~isempty(shortenI)
+        % Crop to the force test
+        rs = shortenData(rs,shortenI);
+        cs = shortenData(cs,shortenI);
+    end
+
+    % Fit the force data
+    bestFit = fitForceData(rs);
+    display(['Best fit force offset: ' num2str(bestFit.y0)]);
+    display(['Best fit spring constant: ' num2str(bestFit.ks)]);
+
+    % Find the world forces
+    force = rsToWorldForce(rs, bestFit.ks);
+    LFz = bestFit.y0 - force.LF.Fz;
+
+    figure
+    plot(rs.time, rs.forceplateF,'.');
+    hold on
+    %plot(rs.time, cs.fzDes,'b')
+    plot(rs.time, -cs.computeFzL,'r')
+    plot(rs.time, LFz,'g')
+
+    xlabel('Time (s)')
+    ylabel('Force (N)')
+end % fitAndPlotForces
+
+end % forceplateAnalysis
