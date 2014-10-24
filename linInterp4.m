8ff27a3c447477e1078f8908592ed47ac3db6009
Andrew Peekema <apeekema@gmail.com>
2014-10-24 15:18:14 -0700

    Extended the linear interpolation function to four dimensions

--- linInterp4.m
+++ linInterp4.m
@@ -1,12 +1,12 @@
-function interpVal = linInterp4(I,values,R1,R2,R3)
+function interpVal = linInterp4(I,values,R1,R2,R3,R4)
 % A linear interpolation function for 4 dimensions
 % Can't use interpn because matlab coder doesn't support it
 % Interpolation formula from:
 % http://bmia.bmt.tue.nl/people/BRomeny/Courses/8C080/Interpolation.pdf
 % Input
-%   I: Target point
+%   I: Point that will be interpolated to
 %   values: Matrix of values with the same dimensions as the ranges
-%   R1, R2, R3: Ranges that the values matrix covers
+%   R1, R2, R3, R4: Ranges that the values matrix covers
 
 % Bound the target point to the variable range
 I(1) = max(I(1),R1(1));   % Lower bound
@@ -15,31 +15,37 @@ I(2) = max(I(2),R2(1));   % Lower bound
 I(2) = min(I(2),R2(end)); % Upper bound
 I(3) = max(I(3),R3(1));   % Lower bound
 I(3) = min(I(3),R3(end)); % Upper bound
+I(4) = max(I(4),R4(1));   % Lower bound
+I(4) = min(I(4),R4(end)); % Upper bound
 
 % Calculate the upper and lower bounding points given the target point
 % Find the lower point
 ia = sum(R1 <= I(1));
 ib = sum(R2 <= I(2));
 ic = sum(R3 <= I(3));
+id = sum(R4 <= I(4));
 % Bound these points to one less than the maximum vector length
 ia = min(ia,length(R1)-1);
 ib = min(ib,length(R2)-1);
 ic = min(ic,length(R3)-1);
-lowerPoint = [R1(ia) R2(ib) R3(ic)];
+id = min(id,length(R4)-1);
+lowerPoint = [R1(ia) R2(ib) R3(ic) R4(id)];
 % The upper point
-upperPoint = [R1(ia+1) R2(ib+1) R3(ic+1)];
+upperPoint = [R1(ia+1) R2(ib+1) R3(ic+1) R4(id+1)];
 
 % Generate bounding hypercube points
 nPoints = 0;
 for i1 = 0:1
 for i2 = 0:1
 for i3 = 0:1
+for i4 = 0:1
     % Increment the number of points
     nPoints = nPoints + 1;
     % Find the point combination
-    pointCombo(nPoints,:) = [i1 i2 i3];
+    pointCombo(nPoints,:) = [i1 i2 i3 i4];
     % Find the point values
     points(nPoints,:) = pointMash(lowerPoint,upperPoint,pointCombo(nPoints,:));
+end
 end
 end
 end
@@ -58,7 +64,7 @@ for n = 1:nPoints
     oppositePointCombo = ~pointCombo(n,:);
     oppositePoint = pointMash(lowerPoint,upperPoint,oppositePointCombo);
     % Weight the point value based on the hypervolume
-    interpVal = interpVal + pointToValue(oppositePoint,values,R1,R2,R3)*nHypervolume;
+    interpVal = interpVal + pointToValue(oppositePoint,values,R1,R2,R3,R4)*nHypervolume;
 end
 
 
@@ -67,14 +73,15 @@ function p = pointMash(p1,p2,pI)
     p = p1.*~pI + p2.*pI;
 end
 
-function value = pointToValue(p,values,R1,R2,R3)
+function value = pointToValue(p,values,R1,R2,R3,R4)
     % Find the point indices
     p1i = sum(R1 <= p(1));
     p2i = sum(R2 <= p(2));
     p3i = sum(R3 <= p(3));
+    p4i = sum(R4 <= p(4));
 
     % Get the value
-    value = values(p1i,p2i,p3i);
+    value = values(p1i,p2i,p3i,p4i);
 end % pointToValue
 
 function V = hypervolume(p1,p2)
--- toeConstraintLookup.m
+++ toeConstraintLookup.m
@@ -6,25 +6,28 @@ clear all
 close all
 
 % Variable ranges
-R1 = [0:0.5:10];
+R1 = [0:0.5:1];
 R2 = [0:0.5:10];
-R3 = [0:0.5:10];
+R3 = [0:0.5:12];
+R4 = [0:0.5:15];
 
 % Filler values
-values = NaN(length(R1),length(R2),length(R3));
+values = NaN(length(R1),length(R2),length(R3),length(R4));
 for ia = 1:length(R1)
 for ib = 1:length(R2)
 for ic = 1:length(R3)
-    values(ia,ib,ic) = 25;
+for id = 1:length(R4)
+    values(ia,ib,ic,id) = 23;
+end
 end
 end
 end
 
 % Target point
-I = [1 1.2 0.7];
+I = [1 1.2 0.7 3.1];
 
 % Get the interpolated value
-value = linInterp4(I,values,R1,R2,R3)
+value = linInterp4(I,values,R1,R2,R3,R4)
 
 %{
 % The hip angle data
