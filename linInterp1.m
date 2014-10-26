c40c297b41975f5df5e7ca827de13c3b34ae62a7
Andrew Peekema <apeekema@gmail.com>
2014-10-26 14:54:07 -0700

    Custom interpolation function now matches the output of MATLAB's interpn function.  This has been spot-checked for 1, 2, and 4 dimensions.

--- linInterp1.m
+++ linInterp1.m
@@ -1,53 +1,35 @@
-function interpVal = linInterp4(I,values,R1,R2,R3,R4)
-% A linear interpolation function for 4 dimensions
-% Can't use interpn because matlab coder doesn't support it
+function interpVal = linInterp1(I,values,R1)
+% A linear interpolation function for 1 dimension
+% Simplified implementation of linInterp4 for algorithm testing
 % Interpolation formula from:
 % http://bmia.bmt.tue.nl/people/BRomeny/Courses/8C080/Interpolation.pdf
 % Input
 %   I: Point that will be interpolated to
 %   values: Matrix of values with the same dimensions as the ranges
-%   R1, R2, R3, R4: Ranges that the values matrix covers
+%   R1: Range that the values matrix covers
 
 % Bound the target point to the variable range
 I(1) = max(I(1),R1(1));   % Lower bound
 I(1) = min(I(1),R1(end)); % Upper bound
-I(2) = max(I(2),R2(1));   % Lower bound
-I(2) = min(I(2),R2(end)); % Upper bound
-I(3) = max(I(3),R3(1));   % Lower bound
-I(3) = min(I(3),R3(end)); % Upper bound
-I(4) = max(I(4),R4(1));   % Lower bound
-I(4) = min(I(4),R4(end)); % Upper bound
 
 % Calculate the upper and lower bounding points given the target point
 % Find the lower point
 ia = sum(R1 <= I(1));
-ib = sum(R2 <= I(2));
-ic = sum(R3 <= I(3));
-id = sum(R4 <= I(4));
 % Bound these points to one less than the maximum vector length
 ia = min(ia,length(R1)-1);
-ib = min(ib,length(R2)-1);
-ic = min(ic,length(R3)-1);
-id = min(id,length(R4)-1);
-lowerPoint = [R1(ia) R2(ib) R3(ic) R4(id)];
+lowerPoint = R1(ia);
 % The upper point
-upperPoint = [R1(ia+1) R2(ib+1) R3(ic+1) R4(id+1)];
+upperPoint = R1(ia+1);
 
 % Generate bounding hypercube points
 nPoints = 0;
 for i1 = 0:1
-for i2 = 0:1
-for i3 = 0:1
-for i4 = 0:1
     % Increment the number of points
     nPoints = nPoints + 1;
     % Find the point combination
-    pointCombo(nPoints,:) = [i1 i2 i3 i4];
+    pointCombo(nPoints,:) = i1;
     % Find the point values
     points(nPoints,:) = pointMash(lowerPoint,upperPoint,pointCombo(nPoints,:));
-end
-end
-end
 end
 
 % Generate the interpolated value based on the bounding points weighted by
@@ -62,9 +44,9 @@ for n = 1:nPoints
     nHypervolume = pointHypervolume/totalHypervolume;
     % Find the point opposite the current point
     oppositePointCombo = double(~pointCombo(n,:));
-    oppositePoint = pointMash(lowerPoint,upperPoint,oppositePointCombo);
+    oppositePoint = pointMash(lowerPoint,upperPoint,oppositePointCombo)
     % Weight the point value based on the hypervolume
-    interpVal = interpVal + pointToValue(oppositePoint,values,R1,R2,R3,R4)*nHypervolume;
+    interpVal = interpVal + pointToValue(oppositePoint,values,R1)*nHypervolume;
 end
 
 
@@ -73,15 +55,12 @@ function p = pointMash(p1,p2,pI)
     p = p1.*~pI + p2.*pI;
 end
 
-function value = pointToValue(p,values,R1,R2,R3,R4)
+function value = pointToValue(p,values,R1)
     % Find the point indices
     p1i = sum(R1 <= p(1));
-    p2i = sum(R2 <= p(2));
-    p3i = sum(R3 <= p(3));
-    p4i = sum(R4 <= p(4));
 
     % Get the value
-    value = values(p1i,p2i,p3i,p4i);
+    value = values(p1i);
 end % pointToValue
 
 function V = hypervolume(p1,p2)
@@ -97,4 +76,4 @@ function V = hypervolume(p1,p2)
     end
 end % hypervolume
 
-end % linInterp4
+end % linInterp1
--- toeConstraintLookup.m
+++ toeConstraintLookup.m
@@ -6,10 +6,10 @@ clear all
 close all
 
 % Variable ranges
-R1 = [0:0.5:1];
-R2 = [0:0.5:10];
-R3 = [0:0.5:12];
-R4 = [0:0.5:15];
+R1 = [0:1];
+R2 = [0:10];
+R3 = [0:12];
+R4 = [0:15];
 
 % Filler values
 values = NaN(length(R1),length(R2),length(R3),length(R4));
@@ -24,18 +24,36 @@ end
 end
 
 % Target point
-I = [1 1 6.2 3.2];
+I = [1 2 6.4 3.2];
 
-% Get the interpolated value
-%value = linInterp4(I,values,R1,R2,R3,R4)
-%value = interpn(R1,R2,R3,R4,values,I(1),I(2),I(3),I(4))
-value = interpn(R3,R4,squeeze(values(1,1,:,:)),I(3),I(4))
+% 4 dimensional interpolation
+% It is useful to compare single values between linInterp4 and interpn,
+% but they cannot be understood well in a plot.
+value = linInterp4(I,values,R1,R2,R3,R4)
+value = interpn(R1,R2,R3,R4,values,I(1),I(2),I(3),I(4))
 
-% Plot the reduced-order mesh and the interpolated value
-surf(R4,R3,squeeze(values(1,1,:,:)))
-hold on
-plot3(I(4),I(3),value,'-mo','MarkerSize',15)
-hold off
+% TODO: The reduced-order mesh doesn't visualize the state space well
+% enough to debug.  Need to plot variables on full-order robot
+
+%% For Debugging: 2 dimensional interpolation
+%value = interpn(R3,R4,squeeze(values(1,1,:,:)),I(3),I(4))
+%value = linInterp2(I(3:4),squeeze(values(1,1,:,:)),R3,R4)
+%
+%% Plot the reduced-order mesh and the interpolated value
+%surf(R4,R3,squeeze(values(1,1,:,:)))
+%hold on
+%plot3(I(4),I(3),value,'-mo','MarkerSize',15)
+%hold off
+
+%% For Debugging: 1 dimension interpolation
+%%value = interpn(R4,squeeze(values(1,1,1,:)),I(4))
+%value = linInterp1(I(4),squeeze(values(1,1,1,:)),R4)
+%
+%% Plot the reduced-order mesh and the interpolated value
+%plot(R4,squeeze(values(1,1,1,:)),'.')
+%hold on
+%plot(I(4),value,'-mo','MarkerSize',15)
+%hold off
 
 %{
 % The hip angle data
