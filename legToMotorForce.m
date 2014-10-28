966e5d399c944b5ae5382e58c9ab1e594e26400a
Andrew Peekema <apeekema@gmail.com>
2014-10-27 22:19:27 -0700

    Trying to make wrench transformations in SE3

--- legToMotorForce.m
+++ legToMotorForce.m
@@ -5,13 +5,10 @@ clc
 clear all
 close all
 
-syms qA qB real
+syms qA qB lA lB real
 f0  = SE3;
-l7  = SerialLink(f0, [0 0 -qA], 0.5, 0.015); % A
-l8  = SerialLink(l7.h1f0, [0 0 qA-qB], 0.5, 0.015);
-
-l7.h1f0.g
-l8.h1f0.g
+l7  = SerialLink(f0, [0 0 -qA], lA, 0.015); % A
+l8  = SerialLink(l7.h1f0, [0 0 qA-qB], lB, 0.015);
 
 J(1,1) = diff(l8.h1f0.x,qA);
 J(1,2) = diff(l8.h1f0.x,qB);
@@ -21,4 +18,18 @@ J(2,2) = diff(l8.h1f0.y,qB);
 % Motor forces in terms of end effector forces:
 % tau = J'*F
 
-% TODO: Use purely SE3 calculations
+% The equivalent matrix using properties of SE3:
+syms q1 q2 l1 l2 Fx Fy real
+% Wrench applied to the end effector
+W1 = [Fx; Fy; 0; 0; 0; 0];
+% A frame aligned with the world at the end effector w.r.t. the end of link 1
+F1 = SE3([0 0 0 0 0 q2])*SE3([l2 0 0])*SE3([0 0 0 0 0 -q2-q1]);
+% Translate the wrench from the end effector to the knee
+W2 = F1.transAdj*W1;
+% TODO: Sanity check W2
+%% Remove torque from the available degree of freedom
+%W2 = W2.*[1; 1; 1; 1; 1; 0];
+%% A frame aligned with the knee w.r.t. the world
+%F2 = SE3([0 0 0 0 0 q1])*SE3([l1 0 0]);
+%% Translate the wrench along link A
+%W3 = F2.transAdj*W2;
