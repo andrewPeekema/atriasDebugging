b3cbd004a51b29c5645e9c3f276012db5fb83421
Andrew Peekema <apeekema@gmail.com>
2015-01-11 18:33:43 -0800

    Updated the 11-step vpp analysis to only include the free steps

--- loadFile.m
+++ loadFile.m
@@ -2,4 +2,4 @@
 filePath = '/Users/andrew/Documents/MATLAB/atriasDebugging/data/2014-03-31-10-49-04-vpp-walking-11-steps/atrias_2014-03-31-10-49-04.mat';
 [c rs cs] = logfileToStruct(filePath);
 
-rs = shortenData(rs,[50960:63810]);  % atrias_2014-03-31-10-49-04
+rs = shortenData(rs,[52490:62770]);  % atrias_2014-03-31-10-49-04
